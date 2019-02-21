3be2e44c29ff7781220afa9b276fbc23ceb2e427
GotpagarAshutosh <ashutoshgotpagar@yahoo.in>
2019-02-21 12:14:00 +0530

    Added Static Resources.closes #17

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
@@ -1,13 +1,23 @@
 package guru.springframework.sfgpetclinic;
 
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SfgPetClinicApplication {
+public class SfgPetClinicApplication implements ApplicationRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(SfgPetClinicApplication.class, args);
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        System.out.println("Source args: " + args.getSourceArgs().toString());
+        System.out.println("Option Names: " + args.getOptionNames());
+        System.out.println("Non Option Names: " + args.getNonOptionArgs());
+        System.out.println("Change");
     }
 
 }
