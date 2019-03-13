cf93d949a2709387295c34c1a862eaa415b1a1a1
GotpagarAshutosh <ashutoshgotpagar@yahoo.in>
2019-03-13 11:21:56 +0530

    Added Visit Entity.closes #38

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Visit.java
@@ -1,12 +1,37 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "visit")
 public class Visit extends BaseEntity{
 
+    @Column(name = "date")
     private LocalDate date;
+
+    @Column(name = "description")
     private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private Pet pet;
+
+    public Pet getPet() {
+        return pet;
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
     public LocalDate getDate() {
         return date;
